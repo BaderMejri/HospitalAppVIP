363b9932ca31792a4d15d371a4750be86ac5cbc1
BaderMejri <mejribadreddine1993enc@gmail.com>
2023-09-10 16:41:59 +0100

    Improve project Vue and add new features to the Controller

--- demo/src/main/java/com/badr/demo/repositories/PatientRepository.java
+++ demo/src/main/java/com/badr/demo/repositories/PatientRepository.java
@@ -1,7 +1,10 @@
 package com.badr.demo.repositories;
 
 import com.badr.demo.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    Page<Patient> findByNameContains(String Keyword, Pageable pageable);
 }
--- demo/src/main/java/com/badr/demo/web/PatientController.java
+++ demo/src/main/java/com/badr/demo/web/PatientController.java
@@ -4,9 +4,13 @@ package com.badr.demo.web;
 import com.badr.demo.entities.Patient;
 import com.badr.demo.repositories.PatientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +21,34 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping(path="/index")
-    public String patients(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("ListPatients",patients);
+    public String patients(Model model,
+                           @RequestParam(name="page",defaultValue = "0") int page,
+                           @RequestParam(name="size",defaultValue = "5") int size,
+                           @RequestParam(name="Keyword",defaultValue = "") String Keyword
+    ){
+        Page<Patient> pagePatients = patientRepository.findByNameContains(Keyword, PageRequest.of(page, size));
+        model.addAttribute("ListPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("Keyword", Keyword);
+
        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id, String Keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&Keyword="+Keyword;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> ListPatients(){
+        return patientRepository.findAll();
     }
 }
