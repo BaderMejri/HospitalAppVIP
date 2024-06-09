bc1685a6676607cd7e3970d158fc58e329cbfaf8
BaderMejri <mejribadreddine1993enc@gmail.com>
2024-06-09 22:00:26 +0100

    fix Privilege escalation Security Problem et reglage des differents couches securite de l'app

--- demo/src/main/java/com/badr/demo/security/SecurityConfiguration.java
+++ demo/src/main/java/com/badr/demo/security/SecurityConfiguration.java
@@ -29,7 +29,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
          http.formLogin();
+         http.authorizeHttpRequests().antMatchers("/delete/**","/edit/**","/save/**","/formPatients/**","/editPatients/**").hasRole("ADMIN");
+         http.authorizeHttpRequests().antMatchers("/index/**").hasRole("USER");
          http.authorizeHttpRequests().anyRequest().authenticated();
+         http.exceptionHandling().accessDeniedPage("/403");
     }
 
     @Bean
