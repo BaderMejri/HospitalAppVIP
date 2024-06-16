99db0ccf44ffc5616fd72b2a25622766b6359253
BaderMejri <mejribadreddine1993enc@gmail.com>
2024-06-16 01:04:04 +0100

    Basculer de inMemoryAuthentication vers jdbcAuthentication

--- demo/src/main/java/com/badr/demo/security/SecurityConfiguration.java
+++ demo/src/main/java/com/badr/demo/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.badr.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,20 +11,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder passwordEncoder= passwordEncoder();
-        String encodedPassword = passwordEncoder.encode("1234");
+        /*String encodedPassword = passwordEncoder.encode("1234");
+        System.out.println(encodedPassword);
          auth.inMemoryAuthentication()
                  .withUser("user1").password(encodedPassword).roles("USER");
         auth.inMemoryAuthentication()
                 .withUser("user2").password(passwordEncoder.encode("1111")).roles("USER");
         auth.inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder.encode("1231")).roles("USER","ADMIN");
+                .withUser("admin").password(passwordEncoder.encode("1231")).roles("USER","ADMIN");*/
+        auth.jdbcAuthentication()
+                .dataSource(dataSource).usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
     }
 
     @Override
